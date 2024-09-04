7a0f8cdf264f128e12c9de37271e38624f2aa49c
Marius-P1 <marius.pain@epitech.eu>
2024-09-04 19:00:19 +0200

    feat(index.ts) : Add login route and controller

--- backend/index.ts
+++ backend/index.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import { PrismaClient } from '@prisma/client'
-import { v4 as uuidv4 } from 'uuid';
-
-const request = require('request')
-const prisma = new PrismaClient()
 
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
 
+const userController = require("./controllers/user");
+const authMiddleware = require("./middleware/auth");
+
+app.post("/login", userController.login);
+
 app.get("/", (request: Request, response: Response) => {
 	response.status(200).send("Hello World");
+});
+
+
+// Example of a protected route (You can only access this route if you have a valid token)
+// The authMiddleware function checks if the token is valid
+app.get("/protected", authMiddleware, (request: Request, response: Response) => {
+	response.status(200).send("Protected route");
 });
 
 app.listen(PORT, () => {
