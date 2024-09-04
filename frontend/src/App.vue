6b83b6a1a699534049b9782e00c476aa6fb6420c
6im0n <simon.gl3135@orange.fr>
2024-09-04 15:12:01 +0200

    feat(frontend): Add the App and default logo

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
+import InputText from 'primevue/inputtext';
+
+export default defineComponent({
+  components: {
+    InputText
+  },
+  setup() {
+    const useremail = ref('');
+    const userpassword = ref('');
+    return { useremail, userpassword };
+  }
+});
+</script>
+
+<template>
+  <div class="container">
+    <span class="p-float-label">
+      <InputText id="email" v-model="useremail" />
+      <label for="email">Enter your e-mail</label>
+    </span>
+    <span class="p-float-label">
+      <InputText id="password" v-model="userpassword" />
+      <label for="password">Enter your e-mail</label>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+</style>
