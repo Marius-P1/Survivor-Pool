3be5bb25074f240e93b332ae2dc13fe9f13d4556
6im0n <simon.gl3135@orange.fr>
2024-09-05 09:25:49 +0200

    feat(frontend): Add sidebar menu component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import PrimeVue from 'primevue/config'
+import PrimeVue, {usePrimeVue} from 'primevue/config'
 import Button from "primevue/button"
 import InputText from 'primevue/inputtext';
 import Menubar from 'primevue/menubar';
+import Ripple from "primevue/ripple";
+import StyleClass from 'primevue/styleclass';
+import Image from 'primevue/image';
 
 // Import css
 import './assets/main.css'
@@ -11,11 +14,22 @@ import './assets/main.css'
 // Import router
 import router from './router'
 
+// Import components
+import AppSidebar from "@/components/AppSidebar.vue";
+
 
 const app = createApp(App);
-app.use(PrimeVue);
+
+app.use(PrimeVue, {ripple: true});
 app.use(router);
+
+app.directive('ripple', Ripple);
+app.directive('styleclass', StyleClass);
+
+app.component('PrimeImage', Image);
+app.component('AppSidebar', AppSidebar);
 app.component('PrimeMenubar', Menubar);
 app.component('PrimeButton', Button);
 app.component('InputText', InputText);
+
 app.mount('#app')
