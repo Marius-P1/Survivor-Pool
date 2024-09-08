0bfe19c4b68b0c4c73ea81d91987b0b479e54dd5
6im0n <simon.gl3135@orange.fr>
2024-09-08 17:58:58 +0200

    feat(frontend): update the astrological page

--- frontend/src/pages/AppAstrological.vue
+++ frontend/src/pages/AppAstrological.vue
@@ -4,24 +4,23 @@
     <!-- Circular customers Images -->
 
     <!-- PrimeDropdowns for Selecting Zodiac Signs -->
-    <div class="flex flex-row gap-8">
-      <div class="flex flex-column">
+    <div class="flex flex-row gap-2">
+      <div class="flex flex-column flex-1">
         <div class="mx-1">
           <div class="card">
             <PrimePanel toggleable>
               <template #header>
                 <div class="flex align-items-center gap-2">
-                  <PrimeAvatar :image="selectedCustomer1 ? getCustomerImage(selectedCustomer1) : 'https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png'" size="large" shape="circle" />
+                  <PrimeAvatar v-if="customerImage1" :image="customerImage1" size="large" shape="circle" />
                   <span class="font-bold">{{ selectedCustomer1 ? selectedCustomer1.fullName : 'Customer Name' }}</span>
                 </div>
               </template>
               <template #footer>
                 <div class="flex flex-wrap align-items-center justify-content-between gap-3">
                   <div class="flex align-items-center gap-2">
-                    <PrimeButton icon="pi pi-user" rounded text></PrimeButton>
-                    <PrimeButton icon="pi pi-bookmark" severity="secondary" rounded text></PrimeButton>
+                    <PrimeButton icon="pi pi-search" rounded text></PrimeButton>
                   </div>
-                  <span class="p-text-secondary">Updated 2 hours ago</span>
+                  <span class="p-text-secondary" v-if="selectedCustomer1" >Zodiac: {{customerInfo1.astrological_sign}} </span>
                 </div>
               </template>
               <p class="m-0" v-if="selectedCustomer1">
@@ -38,23 +37,22 @@
             class=""
         />
       </div>
-      <div class="flex flex-column">
+      <div class="flex flex-column flex-1">
         <div class="">
           <div class="card">
             <PrimePanel toggleable>
               <template #header>
                 <div class="flex align-items-center gap-2">
-                  <PrimeAvatar :image="selectedCustomer2 ? getCustomerImage(selectedCustomer2) : 'https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png'" size="large" shape="circle" />
+                  <PrimeAvatar :image="customerImage2" size="large" shape="circle" />
                   <span class="font-bold">{{ selectedCustomer2 ? selectedCustomer2.fullName : 'Customer Name' }}</span>
                 </div>
               </template>
               <template #footer>
                 <div class="flex flex-wrap align-items-center justify-content-between gap-3">
                   <div class="flex align-items-center gap-2">
-                    <PrimeButton icon="pi pi-user" rounded text></PrimeButton>
-                    <PrimeButton icon="pi pi-bookmark" severity="secondary" rounded text></PrimeButton>
+                    <PrimeButton icon="pi pi-search" rounded text></PrimeButton>
                   </div>
-                  <span class="p-text-secondary">Updated 2 hours ago</span>
+                  <span class="p-text-secondary" v-if="selectedCustomer2">Zodiac: {{customerInfo2.astrological_sign}} </span>
                 </div>
               </template>
               <p class="m-0" v-if="selectedCustomer2">
@@ -71,7 +69,7 @@
         />
       </div>
     </div>
-    <PrimeButton label="Check Compatibility" class="mt-4" @click="GetCustomers"/>
+    <PrimeButton label="Check Compatibility" class="mt-4" @click="this.calculateCompatibility()"/>
 
     <!-- Compatibility Percentage -->
     <div v-if="selectedCustomer1 && selectedCustomer2" class="mt-4">
@@ -85,6 +83,7 @@
 import axios from "axios";
 import PrimePanel from "@/main";
 import PrimeAvatar from "@/main";
+import defaultAvatar from '@/assets/default-avatar.jpg';
 
 export default {
   components: {PrimeAvatar, PrimePanel},
@@ -92,6 +91,8 @@ export default {
     return {
       selectedCustomer1: null,
       selectedCustomer2: null,
+      customerImage1: null,
+      customerImage2: null,
       compatibilityPercentage: 0,
       CustomersList: [],
       customerInfo1: {},
@@ -100,24 +101,33 @@ export default {
   },
   watch: {
     selectedCustomer1(newCustomer) {
-      this.calculateCompatibility();
       if (newCustomer) {
         this.getCustomerInfo(newCustomer.id, 'customerInfo1');
+        this.getCustomerImage(newCustomer, 'customerImage1');
       }
     },
     selectedCustomer2(newCustomer) {
-      this.calculateCompatibility();
       if (newCustomer) {
         this.getCustomerInfo(newCustomer.id, 'customerInfo2');
+        this.getCustomerImage(newCustomer, 'customerImage2');
       }
     }
   },
   methods: {
-    getCustomerImage(customer) {
-      if (!customer) {
-        return require('../assets/default_customer.jpg');
+    async getCustomerImage(customer, infoProp) {
+      if(!customer) {
+        this[infoProp] = defaultAvatar;
       }
-      return `data:image/png;base64,http://localhost:3000/customers/${customer.id}/image`;
+      try {
+        const response = await axios.get(`http://localhost:3000/customers/${customer.id}/image`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        this[infoProp] = "data:image/png;base64," + response.data;
+      } catch (error) {
+        console.error('Error fetching customer image:', error);
+      }
     },
     async getCustomerInfo(id, infoProp) {
       try {
@@ -156,6 +166,8 @@ export default {
   },
   mounted() {
     this.GetCustomers();
+    this.getCustomerImage(this.selectedCustomer1, 'customerImage1');
+    this.getCustomerImage(this.selectedCustomer2, 'customerImage2');
   }
 };
 </script>
