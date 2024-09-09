6e019377ca17f3fe38553c822f3ad934218947e2
PerAlban <alban.peralta@epitech.eu>
2024-09-09 10:40:49 +0200

    feat(employeesRoute): Create option to get put and remove in clientlist

--- backend/routes/employee.ts
+++ backend/routes/employee.ts
@@ -13,17 +13,21 @@ async function getEmployeeFromDB(employeeId: number) {
     return employee;
 }
 
+async function custumerIsInDB(constId: number) {
+    const customer = await prisma.customers.findUnique({
+        where: { id: constId }
+    });
+    return customer !== null;
+}
+
 router.get('/', async (req, res) => {
     const employees = await prisma.employee.findMany();
     const employeesWithoutImage = employees.map(employee => {
         return {
             id: employee.id,
-            email: employee.email,
             name: employee.name,
             surname: employee.surname,
-            birthdate: employee.birthdate,
-            gender: employee.gender,
-            work: employee.work
+            birthdate: employee.birthdate
         }
     });
     res.send(employeesWithoutImage);
@@ -58,6 +62,87 @@ router.get('/:id/image', async (req, res) => {
     // @ts-ignore
     const image = Buffer.from(employee.image, 'base64').toString('utf-8');
     res.send(image);
+});
+
+router.get('/:id/customersList', async (req, res) => {
+    const employeeId = req.params.id;
+    const employee = await getEmployeeFromDB(parseInt(employeeId));
+    if (employee === null) {
+        res.status(404).send("Employee not found");
+        return;
+    }
+    let customersDataList = [];
+    for (let i = 0; i < employee.constumerIds.length; i++) {
+        const customer = await prisma.customers.findUnique({
+            where: { id: employee.constumerIds[i] }
+        });
+        if (customer === null) {
+            res.status(404).send("Customer not found");
+            return;
+        }
+        let customerData = {
+            id: customer.id,
+            email: customer.email,
+            name: customer.name,
+            surname: customer.surname,
+            birthdate: customer.birthdate,
+            phone: customer.phone_number,
+            address: customer.address
+        }
+        customersDataList.push(customerData);
+    }
+    res.send(customersDataList);
+});
+
+router.put('/:id/customersList/add/:idconst', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
+    const constId = parseInt(req.params.idconst);
+    const employee = await getEmployeeFromDB(employeeId);
+    if (employee === null) {
+        res.status(404).send("Employee not found");
+        return;
+    }
+    if (!await custumerIsInDB(constId)) {
+        res.status(404).send("Customer not found");
+        return;
+    }
+    if (employee.constumerIds.includes(constId)) {
+        res.status(404).send("Customer already in team");
+        return;
+    }
+    const constumersList = employee.constumerIds;
+    constumersList.push(constId);
+    await prisma.employee.update({
+        where: { id: employeeId },
+        data: { constumerIds: { set: constumersList } }
+    });
+    res.send("Customer added");
+});
+
+router.put('/:id/customersList/remove/:idconst', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
+    const constumerId = parseInt(req.params.idconst);
+    const employee = await getEmployeeFromDB(employeeId);
+    if (employee === null) {
+        res.status(404).send("Employee not found");
+        return;
+    }
+    if (!await custumerIsInDB(constumerId)) {
+        res.status(404).send("Customer not found");
+        return;
+    }
+    const constumersList = employee.constumerIds;
+    const index = constumersList.indexOf(constumerId);
+    if (index > -1) {
+        constumersList.splice(index, 1);
+        await prisma.employee.update({
+            where: { id: employeeId },
+            data: { constumerIds: { set: constumersList } }
+        });
+        res.send("Customer removed");
+    } else {
+        res.status(404).send("Customer not found");
+    }
 });
 
 export default router;
