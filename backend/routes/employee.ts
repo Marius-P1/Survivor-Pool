73a90b20128646559f7866f0b06c380b5a236524
PerAlban <alban.peralta@epitech.eu>
2024-09-09 15:26:59 +0200

    refactor(employeesRoute): Create new route for Manager to manage client

--- backend/routes/employee.ts
+++ backend/routes/employee.ts
@@ -24,6 +24,34 @@ async function getIdFromRequest(req: any) {
     return 1;
 }
 
+async function getEmployeeConstumerList(employee: any, res: any) {
+    if (employee === null) {
+        res.status(404).send("Employee not found");
+        return null;
+    }
+    let customersDataList = [];
+    for (let i = 0; i < employee.constumerIds.length; i++) {
+        const customer = await prisma.customers.findUnique({
+            where: { id: employee.constumerIds[i] }
+        });
+        if (customer === null) {
+            res.status(404).send("Customer not found");
+            return;
+        }
+        let customerData = {
+            id: customer.id,
+            email: customer.email,
+            name: customer.name,
+            surname: customer.surname,
+            birthdate: customer.birthdate,
+            phone: customer.phone_number,
+            address: customer.address
+        }
+        customersDataList.push(customerData);
+    }
+    res.send(customersDataList);
+}
+
 router.get('/', async (req, res) => {
     const employees = await prisma.employee.findMany();
     const employeesWithoutImage = employees.map(employee => {
@@ -73,37 +101,21 @@ router.get('/:id/image', async (req, res) => {
     res.status(200).type('image/png').send(employee.image);
 });
 
+//Employee's request for his customers list
 router.get('/customerslist', async (req, res) => {
     const employee = await getEmployeeFromDB(await getIdFromRequest(req));
-    if (employee === null) {
-        res.status(404).send("Employee not found");
-        return;
-    }
-    let customersDataList = [];
-    for (let i = 0; i < employee.constumerIds.length; i++) {
-        const customer = await prisma.customers.findUnique({
-            where: { id: employee.constumerIds[i] }
-        });
-        if (customer === null) {
-            res.status(404).send("Customer not found");
-            return;
-        }
-        let customerData = {
-            id: customer.id,
-            email: customer.email,
-            name: customer.name,
-            surname: customer.surname,
-            birthdate: customer.birthdate,
-            phone: customer.phone_number,
-            address: customer.address
-        }
-        customersDataList.push(customerData);
-    }
-    res.send(customersDataList);
+    await getEmployeeConstumerList(employee, res);
 });
 
-router.put('customerslist/add/:idconst', async (req, res) => {
-    const employeeId = await getIdFromRequest(req);
+//Managers request for the customers list of one employee
+router.get('/:id/customerslist', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
+    const employee = await getEmployeeFromDB(employeeId);
+    await getEmployeeConstumerList(employee, res);
+});
+
+router.put('/:id/customerslist/add/:idconst', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
     const constId = parseInt(req.params.idconst);
     const employee = await getEmployeeFromDB(employeeId);
     if (employee === null) {
@@ -127,8 +139,8 @@ router.put('customerslist/add/:idconst', async (req, res) => {
     res.send("Customer added");
 });
 
-router.put('/customerslist/remove/:idconst', async (req, res) => {
-    const employeeId = await getIdFromRequest(req);
+router.put('/:id/customerslist/remove/:idconst', async (req, res) => {
+    const employeeId = parseInt(req.params.id);
     const constumerId = parseInt(req.params.idconst);
     const employee = await getEmployeeFromDB(employeeId);
     if (employee === null) {
