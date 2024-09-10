dff466aea91929056d76ddc58f840908e6d51cdb
PerAlban <alban.peralta@epitech.eu>
2024-09-10 16:18:16 +0200

    feat(controllers): Create some function to verify if the token is real

---
+++ backend/controllers/tokenCheck.ts
@@ -0,0 +1,56 @@
+import { PrismaClient } from '@prisma/client'
+import dotenv from "dotenv";
+
+const prisma = new PrismaClient()
+
+dotenv.config();
+
+export async function checkIfTokenIsValid(token: string) {
+    const user = await prisma.token.findUnique({
+        where: {
+            token: token
+        }
+    });
+
+    return user !== null;
+}
+
+export async function isManager(token: string) {
+    const user = await prisma.token.findUnique({
+        where: {
+            token: token
+        }
+    });
+    if (user === null) {
+        return false;
+    }
+    const employee = await prisma.employee.findUnique({
+        where: {
+            email: user.email
+        }
+    });
+    if (employee === null) {
+        return false;
+    }
+    return employee.role === "MANAGER";
+}
+
+export async function getEmployeeIdFromToken(token: string) {
+    const user = await prisma.token.findUnique({
+        where: {
+            token: token
+        }
+    });
+    if (user === null) {
+        return null;
+    }
+    const employee = await prisma.employee.findUnique({
+        where: {
+            email: user.email
+        }
+    });
+    if (employee === null) {
+        return null;
+    }
+    return employee.id;
+}
