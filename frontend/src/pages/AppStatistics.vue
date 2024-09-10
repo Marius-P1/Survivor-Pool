a98cd5912801c6b13f561a2ec473ebcb50391294
6im0n <simon.gl3135@orange.fr>
2024-09-10 21:03:37 +0200

    feat(frontend): wip on the statistic page

--- frontend/src/pages/AppStatistics.vue
+++ frontend/src/pages/AppStatistics.vue
@@ -1,20 +1,228 @@
-<template>
+<template style="height:100%;">
   <div class="px-1 md:px-6">
     <h1>Statistics</h1>
   </div>
   <hr />
-  <div class="flex md:flex-row flex-column gap-2">
-    <div>
+  <div class="flex md:flex-row align-items-stretch flex-column gap-2 h-full">
+    <PrimePanel class="w-6 h-full">
+      <!-- Header -->
+      <template #header>
+        <div class="flex align-items-center gap-2">
+          <PrimeAvatar image="https://primefaces.org/cdn/primevue/images/avatar/amyelsner.png" size="large" shape="circle" />
+          <span class="font-bold">Amy Elsner</span>
+        </div>
+      </template>
 
-    </div>
+      <!-- Content -->
+      <div class="flex flex-column align-items-center justify-content-center">
+
+        <!-- Event type -->
+        <div class="w-full h-full flex align-items-center justify-content-center ">
+          <PrimeChart type="radar" :data="eventChartData" :options="eventChartOptions" class="h-20rem flex align-items-center" />
+        </div>
+
+        <!-- Statistics client nb/ meetings / events -->
+        <div class="flex flex-column justify-content-center md:flex-row gap-4 mb-5">
+          <PrimeCard class="bg-green-500 flex justify-content-center w-4">
+            <template #content>
+              <h5 class="flex justify-content-center align-items-center m-0 pb-1">
+                Number of clients
+              </h5>
+            </template>
+          </PrimeCard>
+          <PrimeCard class="bg-green-500 flex justify-content-center w-4">
+            <template #content>
+              <h5 class="flex justify-content-center align-items-center m-0 pb-1">
+                Events
+              </h5>
+            </template>
+          </PrimeCard>
+          <PrimeCard class="bg-green-500 flex justify-content-center w-4">
+            <template #content>
+              <h5 class="flex justify-content-center align-items-center m-0 pb-1">
+                Meetings
+              </h5>
+            </template>
+          </PrimeCard>
+        </div>
+
+        <!-- Statistics of the payment realised by the coach by month -->
+        <div class=" w-full h-full flex align-items-center ">
+          <PrimeChart type="line" :data="paymentChartData" :options="paymentChartOptions" class="h-10rem w-full" />
+        </div>
+      </div>
+
+      <!-- Footer -->
+      <template #footer>
+        <div class="flex justify-content-end gap-3">
+          <span class="p-text-secondary flex ">Last event: 2 hours ago</span>
+        </div>
+      </template >
+
+    </PrimePanel>
+
   </div>
+
 </template>
 
 <script lang="ts">
+import { ref, onMounted, defineComponent } from "vue";
 
+export default defineComponent({
+  setup() {
+    const eventChartData = ref();
+    const eventChartOptions = ref();
+    const paymentChartData = ref();
+    const paymentChartOptions = ref();
+
+    onMounted(() => {
+      eventChartData.value = setEventChartData();
+      eventChartOptions.value = setEventChartOptions();
+      paymentChartData.value = setPaymentChartData();
+      paymentChartOptions.value = setPaymentChartOptions();
+
+    });
+
+    const setPaymentChartData = () => {
+      const documentStyle = getComputedStyle(document.documentElement);
+
+      return {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--cyan-500'),
+            yAxisID: 'y',
+            tension: 0.4,
+            data: [65, 59, 80, 81, 56, 55, 10]
+          },
+          {
+            label: 'Dataset 2',
+            fill: false,
+            borderColor: documentStyle.getPropertyValue('--gray-500'),
+            yAxisID: 'y1',
+            tension: 0.4,
+            data: [28, 48, 40, 19, 86, 27, 90]
+          }
+        ]
+      };
+    };
+    const setPaymentChartOptions = () => {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      return {
+        stacked: false,
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          },
+          y: {
+            type: 'linear',
+            display: true,
+            position: 'left',
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              color: surfaceBorder
+            }
+          },
+          y1: {
+            type: 'linear',
+            display: true,
+            position: 'right',
+            ticks: {
+              color: textColorSecondary
+            },
+            grid: {
+              drawOnChartArea: false,
+              color: surfaceBorder
+            }
+          }
+        }
+      };
+    }
+
+    const setEventChartData = () => {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+
+      return {
+        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            borderColor: documentStyle.getPropertyValue('--bluegray-400'),
+            pointBackgroundColor: documentStyle.getPropertyValue('--bluegray-400'),
+            pointBorderColor: documentStyle.getPropertyValue('--bluegray-400'),
+            pointHoverBackgroundColor: textColor,
+            pointHoverBorderColor: documentStyle.getPropertyValue('--bluegray-400'),
+            data: [65, 59, 90, 81, 56, 55, 40]
+          },
+          {
+            label: 'My Second dataset',
+            borderColor: documentStyle.getPropertyValue('--pink-400'),
+            pointBackgroundColor: documentStyle.getPropertyValue('--pink-400'),
+            pointBorderColor: documentStyle.getPropertyValue('--pink-400'),
+            pointHoverBackgroundColor: textColor,
+            pointHoverBorderColor: documentStyle.getPropertyValue('--pink-400'),
+            data: [28, 48, 40, 19, 96, 27, 100]
+          }
+        ]
+      };
+    };
+
+    const setEventChartOptions = () => {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+
+      return {
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor
+            }
+          }
+        },
+        scales: {
+          r: {
+            grid: {
+              color: textColorSecondary
+            }
+          }
+        }
+      };
+    };
+    return { eventChartData, eventChartOptions, paymentChartData, paymentChartOptions };
+  }
+});
 </script>
 
 <style scoped>
 
-
+.card {
+  background: var(--surface-card);
+  padding: 2rem;
+  border-radius: 10px;
+  margin-bottom: 1rem;
+}
 </style>
