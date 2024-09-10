64eb2b3a1d5fe91f88bdb57d716e81920e2587d8
PerAlban <alban.peralta@epitech.eu>
2024-09-10 10:19:20 +0200

    refactor(tipsRoute): add try catch to avoid crash if fail request

--- backend/routes/tips.ts
+++ backend/routes/tips.ts
@@ -7,75 +7,95 @@ const prisma = new PrismaClient();
 dotenv.config();
 
 router.get('/', async (req, res) => {
-    const tips = await prisma.tips.findMany();
-    res.send(tips);
+    try {
+        const tips = await prisma.tips.findMany();
+        res.send(tips);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    const tip = await prisma.tips.findUnique({
-        where: {
-            id: parseInt(id)
+    try {
+        const { id } = req.params;
+        const tip = await prisma.tips.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        if (!tip) {
+            res.status(404).send('Tip not found');
+            return;
         }
-    });
-    if (!tip) {
-        res.status(404).send('Tip not found');
-        return;
+        res.send(tip);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    res.send(tip);
 });
 
 router.post('/', async (req, res) => {
-    const maxId = await prisma.tips.findFirst({
-        select: {
-            id: true
-        },
-        orderBy: {
-            id: 'desc'
-        }
-    });
-    const { title, tip } = req.body;
-    const newTip = await prisma.tips.create({
-        data: {
-            id: maxId ? maxId.id + 1 : 1,
-            title: title,
-            tip: tip
-        }
-    });
-    res.send(newTip);
+    try {
+        const maxId = await prisma.tips.findFirst({
+            select: {
+                id: true
+            },
+            orderBy: {
+                id: 'desc'
+            }
+        });
+        const { title, tip } = req.body;
+        const newTip = await prisma.tips.create({
+            data: {
+                id: maxId ? maxId.id + 1 : 1,
+                title: title,
+                tip: tip
+            }
+        });
+        res.send(newTip);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 router.put('/:id', async (req, res) => {
-    const { id } = req.params;
-    const { newTitle, newTip } = req.body;
-    const updatedTip = await prisma.tips.update({
-        where: {
-            id: parseInt(id)
-        },
-        data: {
-            title: newTitle,
-            tip: newTip
+    try {
+        const { id } = req.params;
+        const { newTitle, newTip } = req.body;
+        const updatedTip = await prisma.tips.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                title: newTitle,
+                tip: newTip
+            }
+        });
+        if (!updatedTip) {
+            res.status(404).send('Tip not found');
+            return;
         }
-    });
-    if (!updatedTip) {
-        res.status(404).send('Tip not found');
-        return;
+        res.send(updatedTip);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    res.send(updatedTip);
 });
 
 router.delete('/:id', async (req, res) => {
-    const { id } = req.params;
-    const deletedTip = await prisma.tips.delete({
-        where: {
-            id: parseInt(id)
+    try {
+        const { id } = req.params;
+        const deletedTip = await prisma.tips.delete({
+            where: {
+                id: parseInt(id)
+            }
+        });
+        if (!deletedTip) {
+            res.status(404).send('Tip not found');
+            return;
         }
-    });
-    if (!deletedTip) {
-        res.status(404).send('Tip not found');
-        return;
+        res.send('Tip ' + id + ' deleted');
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    res.send('Tip ' + id + ' deleted');
 });
 
 export default router;
