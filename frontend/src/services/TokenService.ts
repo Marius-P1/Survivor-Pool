50e341ce6bfdc671d7f10f2e6a6ac543720b9731
Marius-P1 <marius.pain@epitech.eu>
2024-09-12 02:02:29 +0200

    feat(frontend): Add TokenService for API authorization

---
+++ frontend/src/services/TokenService.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const checkToken = async () => {
+    console.log('Checking token');
+    try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            return false;
+        }
+        const valid = await axios.get('http://localhost:3000/token/isvalid', {
+            headers : {
+            Authorization: `Bearer ${token}`
+            }
+        });
+        if (!valid.data) {
+            localStorage.removeItem('token');
+            return false;
+        }
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+export default checkToken;
